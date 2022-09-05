0e13f31412164ab0ace81f669e02a6c4cf65579e
unknown <joshuamorrison623@gmail.com>
2022-09-05 19:28:41 -0400

    lightswitch

---
+++ lightswitch/script.js
@@ -0,0 +1,6 @@
+function bulb_on(){
+    document.getElementById('bulb').src='https://toppng.com/uploads/preview/light-bulb-png-transparent-light-bulb-115628766421gdl0skp2k.png';
+}
+function bulb_off(){
+    document.getElementById('bulb').src='https://toppng.com/uploads/preview/light-bulb-on-off-png-11553940208oq66nq8jew.png';
+}
