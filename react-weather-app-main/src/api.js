f8873675d3ee422eade1d9f85ecf97284b5fdaaf
unknown <joshuamorrison623@gmail.com>
2022-10-30 23:18:30 -0400

    react-weather-app-main

---
+++ react-weather-app-main/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "254d1e560cmshe342e2809411e39p150709jsnb5c19fd5cef5",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "2129b213393a498bbf38806f554fb03f"; // enter your key from openweather API
